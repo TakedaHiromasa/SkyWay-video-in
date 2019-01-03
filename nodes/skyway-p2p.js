33bdeaf45cd293a6498bd5a8c6e0abe7d1f8e0e4
TakedaHiromasa <reich893@yahoo.co.jp>
2019-01-03 14:17:19 +0900

    first commit

---
+++ nodes/skyway-p2p.js
@@ -0,0 +1,57 @@
+module.exports = function(RED) {
+  "use strict";
+  var fs = require('fs');
+
+  var key = {
+    key: "",
+    debug: 3,
+  };
+
+  var opt = {
+    peerId: "",
+    autoCall: false,
+    width: 320,
+    height: 240
+  };
+
+  function SkyWayP2PNode(n) {
+    RED.nodes.createNode(this, n);
+
+    this.name = n.name;
+    key.key = n.key;
+    key.debug = n.debug;
+    opt.peerId = n.peerId,
+    opt.autoCall = n.autoCall;
+    opt.width = n.width;
+    opt.height = n.height;
+
+    var node = this;
+
+    RED.httpNode.post('/peer', function(req, res) {
+      var msg = {};
+      msg.payload = req.body.peerId;
+      node.send([null, msg]);
+
+      var rejson = JSON.stringify(req.body);
+      res.send(rejson);
+    });
+
+    RED.httpNode.get('/js/SkyWay.js', function(req, res) {
+      var head = "";
+      head += "var key=" + JSON.stringify(key);
+      head += "\nvar opt=" + JSON.stringify(opt);
+      res.send(head + fs.readFileSync('skyway.js', 'utf-8'));
+    });
+
+    // RED.httpNode.get('/style.css', function(req, res) {
+    //   res.send(fs.readFileSync('style.css', 'utf-8'));
+    // });
+
+    this.on('input', function(msg) {
+      msg.payload = fs.readFileSync('skyway.html', 'utf-8');
+      node.send([msg, null]);
+    });
+
+  }
+  RED.nodes.registerType("skyway-p2p", SkyWayP2PNode);
+}
